9c143b85ee7143b8cf954d0a472e84b1ef77e2d3
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-09 18:24:27 -0500

    Satisfy test suites (inefficiently)

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,20 @@
+fn has_factors(n: u32, values: &[u32]) -> bool {
+    for v in values {
+        if n % v == 0 {
+            return true;
+        }
+    }
+    false
+}
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes: Vec<u32> = vec![];
+    let mut num = 2u32;
+    while primes.len() <= (n as usize) {
+        while has_factors(num, &primes) {
+            num += 1;
+        }
+        primes.push(num)
+    }
+    *primes.last().unwrap()
 }
