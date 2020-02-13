b989dd2433171e3d9b11fca14ef68a1680396f03
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-13 07:10:36 -0500

    Solve in an even more straightforward way

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,5 +1,5 @@
 def convert(n) -> str:
-    sound = "{}{}{}".format(
-        "" if n % 3 else "Pling", "" if n % 5 else "Plang", "" if n % 7 else "Plong",
-    )
+    sound = "" if n % 3 else "Pling"
+    sound += "" if n % 5 else "Plang"
+    sound += "" if n % 7 else "Plong"
     return sound or str(n)
