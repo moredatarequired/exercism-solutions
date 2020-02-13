df6d390f19a006471d1f70d488a1c14906684ac3
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-13 07:07:05 -0500

    Solve without using an unecessary regex

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,7 +1,5 @@
-import re
-
-
 def convert(n) -> str:
-    sound = f"{n % 3 or 'Pling'}{n % 5 or 'Plang'}{n % 7 or 'Plong'}"
-    sound = re.sub(r"\d", "", sound)
-    return sound if sound else str(n)
+    sound = "{}{}{}".format(
+        "" if n % 3 else "Pling", "" if n % 5 else "Plang", "" if n % 7 else "Plong",
+    )
+    return sound or str(n)
