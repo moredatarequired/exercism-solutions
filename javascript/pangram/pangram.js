6f51ec3ea11b796a5516740dabeb3dc608301f12
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-29 20:49:02 -0500

    Solve pangram in javascript

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,5 +1,3 @@
-export const isPangram = phrase => {
-  const stripped = phrase.toLowerCase().replace(/[^a-z]/g, "");
-  const letters = new Set([...stripped]);
-  return 26 == letters.size;
-};
+const letters = str => new Set([...str.toLowerCase().replace(/[^a-z]/g, "")]);
+
+export const isPangram = phrase => 26 === letters(phrase).size;
